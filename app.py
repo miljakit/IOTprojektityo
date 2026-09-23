b68ab71ce05e787d1a80c0bbd31c16063394b255
Milja Kitinoja <milja.kitinoja@seamk.fi>
2026-09-23 23:31:47 +0300

    korjattu sisennykset välilyönneiksi

--- app.py
+++ app.py
@@ -17,79 +17,79 @@ def home():
 
 @app.route("/api/measurement", methods=["POST"])
 def get_measurement():
-	data = request.get_json()
-	temp = data[0]
-	humi = data[1] 
+    data = request.get_json()
+    temp = data[0]
+    humi = data[1] 
 
     #measurements.append(data)
     print("saatiin mittaus")
     print(data)
 
-	conn = sqlite3.connect("sensoridata.db")
-	cursor = conn.cursor()
+    conn = sqlite3.connect("sensoridata.db")
+    cursor = conn.cursor()
 
-	cursor.execute("""
-			INSERT INTO mittaukset (temp, humi)
-			VALUES (?, ?)
-			""", (temp, humi))
-			conn.commit()
-			conn.close()
-			
+    cursor.execute("""
+            INSERT INTO mittaukset (temp, humi)
+            VALUES (?, ?)
+            """, (temp, humi))
+            conn.commit()
+            conn.close()
+            
     data_json = json.dumps(data)
 
     return data_json
 
 @app.route("/api/showmeasurements")
 def show_measurements():
-	paivamaara = request.args.get("date")
-	
-	conn = sqlite3.connect("sensoridata.db")
-	cursor = conn.cursor()
-	
-	cursor.execute("""
-		SELECT aika, lampotila, kosteus
-		FROM mittaukset
-		WHERE date(aika) = ?
-		ORDER BY aika
-	""", (paivamaara,))
-	
-	rows = cursor.fetchall()
-	conn.close()
-	
-	return jsonify(rows)
+    paivamaara = request.args.get("date")
+    
+    conn = sqlite3.connect("sensoridata.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT aika, lampotila, kosteus
+        FROM mittaukset
+        WHERE date(aika) = ?
+        ORDER BY aika
+    """, (paivamaara,))
+    
+    rows = cursor.fetchall()
+    conn.close()
+    
+    return jsonify(rows)
     
 @app.route("/api/latestmeasurements")
 def latest_measurements():
-	conn = sqlite3.connect("sensoridata.db")
-	cursor = conn.cursor()
-	
-	cursor.execute("""
-		SELECT aika, lampotila, kosteus
-		FROM mittaukset
-		WHERE datetime(aika) >= datetime('now', '-6 hours')
-		ORDER BY aika
-	""")
-		
-	rows = cursor.fetchall()
-	conn.close()
-		
-	return jsonify(rows)
-	
+    conn = sqlite3.connect("sensoridata.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT aika, lampotila, kosteus
+        FROM mittaukset
+        WHERE datetime(aika) >= datetime('now', '-6 hours')
+        ORDER BY aika
+    """)
+        
+    rows = cursor.fetchall()
+    conn.close()
+        
+    return jsonify(rows)
+    
 @app.route("/api/availabledates")
 def available_dates():
-	conn = sqlite3.connect("sensoridata.db")
-	cursor = conn.cursor()
-	
-	cursor.execute("""
-		SELECT DISTINCT date(aika)
-		FROM mittaukset
-		ORDER BY date(aika)
-	""")
-	
-	dates = [row[0] for row in cursor.fetchall()]
-	conn.close()
-	
-	return jsonify(dates)
+    conn = sqlite3.connect("sensoridata.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT DISTINCT date(aika)
+        FROM mittaukset
+        ORDER BY date(aika)
+    """)
+    
+    dates = [row[0] for row in cursor.fetchall()]
+    conn.close()
+    
+    return jsonify(dates)
 
 if __name__ == "__main__":
     app.run(debug=True)
