b507e63f026238c102290866bb373cce8d2cb8e0
Milja Kitinoja <milja.kitinoja@seamk.fi>
2026-09-23 23:20:41 +0300

    lisäys

--- sensor.py
+++ sensor.py
@@ -3,11 +3,9 @@ import time
 from datetime import datetime
 import seeed_dht
 import json
-import sqlite3
+import sqlite3s
 
 sensor = seeed_dht.DHT("11", 12)
-conn = sqlite3.connect("sensoridata.db")
-cursor = conn.cursor()
 print("DHT11 reading every hour, Ctrl+C to quit")
 
 try:
@@ -16,17 +14,12 @@ try:
 			timestamp_ms = int(datetime.now().timestamp()*1000)
 			humi, temp = sensor.read()
 			print(f"DHT11  Humidity {humi:.1f}%  Temperature {temp:.1f}C")
-			cursor.execute("""
-			INSERT INTO mittaukset (lampotila, kosteus)
-			VALUES (?, ?)
-			""", (temp, humi))
-			conn.commit()
 
-			data = [timestamp_ms, temp, humi]
+			data = [temp, humi]
 			response = requests.post("tite25-iot-mkit-anfyhbahf5a0gbb6.switzerlandnorth-01.azurewebsites.net/api/measurement", json=data)
 
 			print(response)
-			time.sleep(60)
+			time.sleep(60) 
 
 except KeyboardInterrupt:
     print("\nBye")
